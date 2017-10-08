926ecb902d239cb9237256cc40da86b01712bd43
Yuanchen Li <yli@y-l.me>
2017-10-07 20:28:02 -0500

    basic_grab

--- leap_motion.py
+++ leap_motion.py
@@ -8,7 +8,25 @@ sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 import Leap
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
+class SampleListener(Leap.Listener):
+
+    def on_connect(self, controller):
+        print "Connected"
+
+
+    def on_frame(self, controller):
+        frame = controller.frame()
+        print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d" % (frame.id, frame.timestamp, len(frame.hands), len(frame.fingers))
+
+
 def main():
+    print 'connecting'
+    # Create a sample listener and controller
+    listener = SampleListener()
+    controller = Leap.Controller()
+
+    # Have the sample listener receive events from the controller
+    controller.add_listener(listener)
 
     # Keep this process running until Enter is pressed
     print "Press Enter to quit..."
