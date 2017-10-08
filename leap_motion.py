5ee5a51384f5147cec3f1fc78558017e4ca87d01
Yuanchen Li <yli@y-l.me>
2017-10-07 19:54:28 -0500

    others

---
+++ leap_motion.py
@@ -0,0 +1,21 @@
+import os, sys, inspect, thread, time
+
+src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+# arch_dir = './lib/x64' if sys.maxsize > 2**32 else './lib/x86'
+arch_dir = './lib'
+sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
+
+import Leap
+from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+
+def main():
+
+    # Keep this process running until Enter is pressed
+    print "Press Enter to quit..."
+    try:
+        sys.stdin.readline()
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
